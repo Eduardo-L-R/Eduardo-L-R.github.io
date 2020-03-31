0659004b525a51e9f297157592d652fb718e7650
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2020-03-31 00:47:33 -0400

    se agrega carpeta build

---
+++ build/precache-manifest.42bd346a5369cda6ece4ad07af7dcb34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0155fb0b338a086bae721f24b91480a4",
+    "url": "/Eduardo-L-R.github.io/index.html"
+  },
+  {
+    "revision": "6762ee2a2222fa74c227",
+    "url": "/Eduardo-L-R.github.io/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "35f04f297b62c0ae6367",
+    "url": "/Eduardo-L-R.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6762ee2a2222fa74c227",
+    "url": "/Eduardo-L-R.github.io/static/js/2.2669d6f3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Eduardo-L-R.github.io/static/js/2.2669d6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f04f297b62c0ae6367",
+    "url": "/Eduardo-L-R.github.io/static/js/main.fc399702.chunk.js"
+  },
+  {
+    "revision": "d2c4b9cba17c673ffa95",
+    "url": "/Eduardo-L-R.github.io/static/js/runtime-main.35dd455f.js"
+  }
+]);
